b6957ba4e43cfd12479118ee03524346246f172c
b1ankk <ttemk22@gmail.com>
2022-08-29 20:37:54 +0200

    Add eslint, prettier, stylelint and reformat project files

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 128,
+    tabWidth: 4,
+    semi: true,
+    trailingComma: 'es5',
+    singleQuote: true,
+    jsxSingleQuote: false,
+    arrowParens: 'avoid',
+    endOfLine: 'lf',
+    importOrder: ['^[^./]*[^.]*[.]css$', '^[.]{1,2}/.*(?<!.css)$', '^[.]{1,2}/.*[.]css$'],
+    importOrderSortSpecifiers: true,
+};
